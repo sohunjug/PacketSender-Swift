360eefc81c8807513eac9745bad4b3a8e52a61cb
sohunjug <sohunjug@hotmail.com>
2016-08-02 18:20:42 +0800

    Initial Commit

---
+++ PacketSender/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  PacketSender
+//
+//  Created by sohunjug on 2016/8/2.
+//  Copyright © 2016年 sohunjug. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
